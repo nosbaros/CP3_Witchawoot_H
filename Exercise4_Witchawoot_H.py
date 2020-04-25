2938061fb3f13a83a2441d96dbebefc4a5dd9047
nosbaros <nosbaros@gmail.com>
2020-04-25 11:42:38 +0700

    Update Exercise4_Witchawoot_H.py

--- Exercise4_Witchawoot_H.py
+++ Exercise4_Witchawoot_H.py
@@ -2,4 +2,8 @@ FoundationEnglish = 10
 GeneralBusiness = 40
 IntroductiontoComputerSystems = 50
 ComputerProgramming = 70
-print("--- Your Scorce ---")
+print("--- Your Score ---")
+print("Foundation English : ", FoundationEnglish)
+print("General Business : ", GeneralBusiness)
+print("Introduction to Computer Systems : ", IntroductiontoComputerSystems)
+print("Computer Programming : ", ComputerProgramming)
